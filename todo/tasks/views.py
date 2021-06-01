539d97d9e757eab5c1cc189695f9f8c06bd657ca
Jimmy290901 <shubham290901@gmail.com>
2021-06-01 20:58:49 +0530

    Read and Create Operations

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import MyTasks
+from .forms import MyTaskForm
 # Create your views here.
 
 def home(request):
-    return render(request, 'tasks/home.html')
+    allTasks = MyTasks.objects.all()
+    taskForm = MyTaskForm()
+
+    if request.method == 'POST':
+        addTask = MyTaskForm(request.POST)
+        if addTask.is_valid():
+            addTask.save()
+
+    context = {
+        'allTasks': allTasks,
+        'taskForm': taskForm,
+    }
+    return render(request, 'tasks/home.html', context)
